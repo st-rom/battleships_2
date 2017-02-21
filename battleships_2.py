c74d0ba956452515f0629d84016e4137e78aec30
Roman Stepaniuk <r.stepaniuk@ucu.edu.ua>
2017-02-21 04:18:16 +0200

    -

--- battleships_2.py
+++ battleships_2.py
@@ -1,3 +1,25 @@
+class Ship():
+    bow
+    horizontal
+    hit
+    def __init__(length):
+        self.length = length
+    def shoot_at(tupl):
+        if mapa[tupl[0]][0][tupl[-1]] == '*':
+            print('You shot the ship!!!')
 
 
-#generate_field()
+class Player:
+    def __init__(self, pl_name):
+        self.pl_name = pl_name
+    def read_position(pos):
+        if pos[0].isupper():
+            spot = (int(pos[1:]) - 1, ord(pos[0]) - 65)
+        else:
+            spot = (int(pos[1:]), ord(pos[0]) - 97)
+        return spot
+
+
+class Field:
+    def __init__(self, ships):
+        self.ships = Ship([[Ship()]])
