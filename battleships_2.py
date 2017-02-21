98613fe9b01e162a8ca49e4aeb7e4ae641b042bf
Roman Stepaniuk <r.stepaniuk@ucu.edu.ua>
2017-02-21 12:39:45 +0200

    -

--- battleships_2.py
+++ battleships_2.py
@@ -1,17 +1,24 @@
 class Ship():
-    bow
-    horizontal
-    hit
-    def __init__(length):
-        self.length = length
-    def shoot_at(tupl):
-        if mapa[tupl[0]][0][tupl[-1]] == '*':
-            print('You shot the ship!!!')
+    def __init__(self, bow, horizontal, length):
+        self.bow = bow
+        self.horizontal = horizontal
+        self._length = length
+        if horizontal is True:
+            self._hit = [False] * length[-1]
+        else:
+            self._hit = [False] * length[0]
 
+    def shoot_at(self, zil):
+        if self.horizontal is False:
+            damaged_spot = zil[0] - self.bow[0]
+        else:
+            damaged_spot = zil[-1] - self.bow[-1]
+        self._hit[damaged_spot] = True
 
 class Player:
     def __init__(self, pl_name):
         self.pl_name = pl_name
+
     def read_position(pos):
         if pos[0].isupper():
             spot = (int(pos[1:]) - 1, ord(pos[0]) - 65)
@@ -22,4 +29,4 @@ class Player:
 
 class Field:
     def __init__(self, ships):
-        self.ships = Ship([[Ship()]])
+        self.ships = [[Ship(self.bow), Ship(self.horizontal), Ship(self.length), Ship(self.hit)]])
